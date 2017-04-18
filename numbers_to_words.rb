cf49c0e29a3b59a2b6bdb6efc1e937abd2658124
Nick Powell and Spencer Ruiz <nick@>
2017-04-18 11:34:35 -0700

    add passing spec for teens through ninety nine.

--- numbers_to_words.rb
+++ numbers_to_words.rb
@@ -1,6 +1,7 @@
 class NumbersToWords
   def transform_number(number)
-    number_values = {
+    ones_values = {
+      0 => "",
       1 => "one",
       2 => "two",
       3 => "three",
@@ -10,6 +11,9 @@ class NumbersToWords
       7 => "seven",
       8 => "eight",
       9 => "nine",
+    }
+
+    teens_values = {
       10 => "ten",
       11 => "eleven",
       12 => "twelve",
@@ -20,19 +24,43 @@ class NumbersToWords
       17 => "seventeen",
       18 => "eighteen",
       19 => "nineteen",
-      20 => "twenty",
-      30 => "thirty",
-      40 => "fourty",
-      50 => "fifty",
-      60 => "sixty",
-      70 => "seventy",
-      80 => "eighty",
-      90 => "ninety",
+    }
+
+    tens_values = {
+      2 => "twenty",
+      3 => "thirty",
+      4 => "fourty",
+      5 => "fifty",
+      6 => "sixty",
+      7 => "seventy",
+      8 => "eighty",
+      9 => "ninety",
+    }
+
+    exponential_values = {
       100 => "hundred",
       1000 => "thousand",
       1000000 => "million",
       1000000000 => "billion",
     }
-    number_values.fetch(number)
+
+    number_word = ""
+
+    numbers = number.to_s.split(//).map{|num| num.to_i}
+
+    if number < 10
+      number_word.concat(ones_values.fetch(numbers[-1]))
+    elsif number < 20
+      number_word.concat(teens_values.fetch(number))
+    elsif number < 100
+      number_word.concat(tens_values.fetch(numbers[-2]))
+      if numbers[-1] > 0
+        number_word.concat(" ")
+        number_word.concat(ones_values.fetch(numbers[-1]))
+      end
+    end
+
+    number_word
+
   end
 end
--- numbers_to_words_spec.rb
+++ numbers_to_words_spec.rb
@@ -9,4 +9,22 @@ describe('numbers_to_words#letter_score') do
     expect(number.transform_number(1)).to(eq("one"))
   end
 
+  it("returns the string 'ten' when passed the number 10") do
+    expect(number.transform_number(10)).to(eq("ten"))
+  end
+
+  it("returns the string 'twenty' when passed the number 20") do
+    expect(number.transform_number(20)).to(eq("twenty"))
+  end
+
+  it("returns the string 'twenty five' when passed the number 25") do
+    expect(number.transform_number(25)).to(eq("twenty five"))
+  end
+
+  xit("returns the string 'one hundred' when passed the number 100") do
+    expect(number.transform_number(100)).to(eq("one hundred"))
+  end
+
+
+
 end
